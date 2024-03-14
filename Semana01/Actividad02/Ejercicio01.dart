803c8db19ff25d24461803e4eb725fb11a5d8b1d
anthony-vilcatoma <anthonyjwvp@gmail.com>
2024-03-14 16:58:19 -0500

    Organizando Carpetas y Terminando Actividad02

---
+++ Semana01/Actividad02/Ejercicio01.dart
@@ -0,0 +1,23 @@
+void main() {
+
+  //Declaración de cadena(String)
+  String name = "Anthony";
+  print('Declaración de tipo String -> $name');
+  
+  //Declaración dinamica con var
+  var name2 = "Juan";
+  print('Declaracion de tipo dinamica var -> $name2');
+  
+  //Declaración dinamic con const
+  //Este se inicializa en compilación
+  const name3 = "Maria";
+  print('Declaracion de tipo dinamica const -> '+name3);
+  
+  
+ //Declaracion dinamica con final
+  //Este se inicializa durante la ejecución
+  const name4 = "Ruben";
+  print('Declaracion de tipo dinamica final ->' + name4);
+
+}
+ 
---
+++ Semana01/Actividad02/Ejercicio02.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  //Declaracion numerica int
+  int numeroEntero = 5;
+  
+  //Declaracion numerica decimal
+  double numeroDecimal = 5.01;
+  
+  print('Numero Entero -> $numeroEntero');
+  print('Numero Decimal -> $numeroDecimal');
+}
+ 
